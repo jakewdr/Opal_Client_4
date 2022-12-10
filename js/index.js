aaec923871958cb4ffb13a496b6d3c3626ac99da
Opal <C92i747A@pm.me>
2022-12-10 12:14:44 +0000

    Added the f9 shortcut

--- js/index.js
+++ js/index.js
@@ -11,55 +11,56 @@ function primaryWindow() {
         width: width,
         height: height,
         webPreferences: {
-            nodeIntegration: true,
-            nodeIntegrationInWorker: true,
+            nodeIntegration: false,
+            nodeIntegrationInWorker: false,
             webSecurity: false,
             contextIsolation: false,
             devTools: true,
-        }
+            sandbox: false
+        },
     });
     // Video ->
-    electron_1.app.commandLine.appendSwitch('disable-gpu-vsync');
-    electron_1.app.commandLine.appendSwitch('disable-frame-rate-limit');
-    electron_1.app.commandLine.appendSwitch('enable-accelerated-2d-canvas');
-    electron_1.app.commandLine.appendSwitch('double-buffer-compositing');
-    electron_1.app.commandLine.appendSwitch('force_high_performance_gpu');
-    electron_1.app.commandLine.appendSwitch('max-gum-fps=9999');
-    // Audio -> 
-    electron_1.app.commandLine.appendSwitch('alsa-enable-upsampling');
-    electron_1.app.commandLine.appendSwitch('audio-process-high-priority');
-    electron_1.app.commandLine.appendSwitch('enable-exclusive-audio');
+    electron_1.app.commandLine.appendSwitch("disable-gpu-vsync");
+    electron_1.app.commandLine.appendSwitch("disable-frame-rate-limit");
+    electron_1.app.commandLine.appendSwitch("enable-accelerated-2d-canvas");
+    electron_1.app.commandLine.appendSwitch("double-buffer-compositing");
+    electron_1.app.commandLine.appendSwitch("force_high_performance_gpu");
+    electron_1.app.commandLine.appendSwitch("max-gum-fps=9999");
+    // Audio ->
+    electron_1.app.commandLine.appendSwitch("alsa-enable-upsampling");
+    electron_1.app.commandLine.appendSwitch("audio-process-high-priority");
+    electron_1.app.commandLine.appendSwitch("enable-exclusive-audio");
     // Networking ->
-    electron_1.app.commandLine.appendSwitch('disable-background-networking');
-    electron_1.app.commandLine.appendSwitch('enable-quic');
+    electron_1.app.commandLine.appendSwitch("disable-background-networking");
+    electron_1.app.commandLine.appendSwitch("enable-quic");
     //General ->
-    electron_1.app.commandLine.appendSwitch('no-sandbox-and-elevated');
-    electron_1.app.commandLine.appendSwitch('disable-low-end-device-mode');
-    electron_1.app.commandLine.appendSwitch('ignore-gpu-blacklist');
+    electron_1.app.commandLine.appendSwitch("no-sandbox-and-elevated");
+    electron_1.app.commandLine.appendSwitch("disable-low-end-device-mode");
+    electron_1.app.commandLine.appendSwitch("ignore-gpu-blacklist");
     // Input ->
-    electron_1.app.commandLine.appendSwitch('allow-pre-commit-input');
-    electron_1.app.commandLine.appendSwitch('disable-third-party-keyboard-workaround');
+    electron_1.app.commandLine.appendSwitch("allow-pre-commit-input");
+    electron_1.app.commandLine.appendSwitch("disable-third-party-keyboard-workaround");
     // Unnecessary Stuff ->
-    electron_1.app.commandLine.appendSwitch('disable-breakpad');
-    electron_1.app.commandLine.appendSwitch('disable-component-update');
-    electron_1.app.commandLine.appendSwitch('disable-print-preview');
-    electron_1.app.commandLine.appendSwitch('disable-metrics');
-    electron_1.app.commandLine.appendSwitch('disable-metrics-repo');
-    electron_1.app.commandLine.appendSwitch('disable-speech-api');
-    electron_1.app.commandLine.appendSwitch('disable-web-security');
-    electron_1.app.commandLine.appendSwitch('disable-hang-monitor');
-    electron_1.app.commandLine.appendSwitch('ignore-gpu-blacklist');
-    electron_1.app.commandLine.appendSwitch('disable-2d-canvas-clip-aa');
-    electron_1.app.commandLine.appendSwitch('disable-bundled-ppapi-flash');
-    electron_1.app.commandLine.appendSwitch('disable-logging');
-    electron_1.app.commandLine.appendSwitch('disable-web-security');
-    electron_1.app.commandLine.appendSwitch('disable-renderer-backgrounding');
-    electron_1.app.commandLine.appendSwitch('disable-background-timer-throttling');
-    electron_1.app.commandLine.appendSwitch('disable-backing-store-limit');
-    electron_1.session.defaultSession.loadExtension(path_1.default.join(__dirname, 'community-patch'));
+    electron_1.app.commandLine.appendSwitch("disable-breakpad");
+    electron_1.app.commandLine.appendSwitch("disable-component-update");
+    electron_1.app.commandLine.appendSwitch("disable-print-preview");
+    electron_1.app.commandLine.appendSwitch("disable-metrics");
+    electron_1.app.commandLine.appendSwitch("disable-metrics-repo");
+    electron_1.app.commandLine.appendSwitch("disable-speech-api");
+    electron_1.app.commandLine.appendSwitch("disable-web-security");
+    electron_1.app.commandLine.appendSwitch("disable-hang-monitor");
+    electron_1.app.commandLine.appendSwitch("ignore-gpu-blacklist");
+    electron_1.app.commandLine.appendSwitch("disable-2d-canvas-clip-aa");
+    electron_1.app.commandLine.appendSwitch("disable-bundled-ppapi-flash");
+    electron_1.app.commandLine.appendSwitch("disable-logging");
+    electron_1.app.commandLine.appendSwitch("disable-web-security");
+    electron_1.app.commandLine.appendSwitch("disable-renderer-backgrounding");
+    electron_1.app.commandLine.appendSwitch("disable-background-timer-throttling");
+    electron_1.app.commandLine.appendSwitch("disable-backing-store-limit");
+    electron_1.session.defaultSession.loadExtension(path_1.default.join(__dirname, "community-patch"));
     win.removeMenu();
     win.loadURL("https://ev.io/");
-    win.webContents.on('before-input-event', (event, input) => {
+    win.webContents.on("before-input-event", (event, input) => {
         if (input.key === "F5") {
             win.reload();
             event.preventDefault();
@@ -76,18 +77,24 @@ function primaryWindow() {
             win.loadURL("https://ev.io/user/login");
             event.preventDefault();
         }
+        if (input.key === "F9") {
+            win.close();
+            electron_1.app.quit();
+        }
         if (input.key === "F11") {
             win.setFullScreen(!win.isFullScreen());
             event.preventDefault();
         }
     });
-    win.once("ready-to-show", () => { win.show(); });
+    win.once("ready-to-show", () => {
+        win.show();
+    });
 }
-electron_1.app.on('ready', () => {
-    console.log('App is ready to run');
+electron_1.app.on("ready", () => {
+    console.log("App is ready to run");
     primaryWindow();
 });
-electron_1.app.on('window-all-closed', () => {
+electron_1.app.on("window-all-closed", () => {
     electron_1.app.quit();
 });
 //# sourceMappingURL=index.js.map
