17dd93fc8a1b4413db0ddcd3da83033fc6c27185
Opal <C92i747A@pm.me>
2022-12-28 10:45:59 +0000

    Added automatic minification

--- js/preload.js
+++ js/preload.js
@@ -1,18 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-function closeApp(e) {
-    e.preventDefault();
-    electron_1.ipcRenderer.send('close');
-}
-electron_1.ipcRenderer.on('menu', () => {
-    document.getElementById("menu_hotkey_top").click();
-});
-document.addEventListener('DOMContentLoaded', () => {
-    if (!document.getElementById('ClientExit')) {
-        document.getElementById('top_bar_cont').innerHTML += `
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const electron_1=require("electron");function closeApp(e){e.preventDefault(),electron_1.ipcRenderer.send("close")}electron_1.ipcRenderer.on("menu",()=>{document.getElementById("menu_hotkey_top").click()}),document.addEventListener("DOMContentLoaded",()=>{document.getElementById("ClientExit")||(document.getElementById("top_bar_cont").innerHTML+=`
     <button type="button" id="ClientExit" class="btn btn-secondary btn-sm" style="position: absolute; left: 0; top: 0; width: 100px !important">Exit</button>
-    `;
-    }
-    document.getElementById('ClientExit').addEventListener('click', closeApp);
-});
+    `),document.getElementById("ClientExit").addEventListener("click",closeApp)});
