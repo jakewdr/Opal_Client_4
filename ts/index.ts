11ba5acb72c92b67abbc789febd5f5c6ed81ee3e
Opal <C92i747A@pm.me>
2023-01-08 11:58:53 +0000

    More performance updates

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,5 @@
 import { app, BrowserWindow, session, screen, clipboard, ipcMain} from "electron";
-import os from 'os';
+var { cpus } = require("os");
 import 'v8-compile-cache'
 import path from "path";
 
@@ -9,24 +9,27 @@ function switches () {
 
   app.commandLine.appendSwitch("disable-gpu-vsync");
   app.commandLine.appendSwitch("disable-frame-rate-limit");
+  if (cpus()[0].model.includes("AMD")) {
+    app.commandLine.appendSwitch("enable-zero-copy");
+  }
   
   // GPU -> 
 
-  app.commandLine.appendSwitch("double-buffer-compositing");
-  app.commandLine.appendSwitch('enable-native-gpu-memory-buffers');
   app.commandLine.appendSwitch("enable-gpu-rasterization");
   app.commandLine.appendSwitch('enable-oop-rasterization');
-  app.commandLine.appendSwitch("ignore-gpu-blacklist");
-  app.commandLine.appendSwitch("disable-accelerated-video-decode", "false");
+  app.commandLine.appendSwitch('enable-native-gpu-memory-buffers');
+  app.commandLine.appendSwitch('enable-gpu-memory-buffer-compositor-resources');
   app.commandLine.appendSwitch("force-gpu-mem-available-mb", '1500');
-  app.commandLine.appendSwitch('force_high_performance_gpu')
-  app.commandLine.appendSwitch('max-gum-fps','9999');
-  
-  // GL Stuff ->
-
+  app.commandLine.appendSwitch("force-gpu-mem-discardable-limit-mb", '500');
   app.commandLine.appendSwitch('enable-webgl2-compute-context');
-  app.commandLine.appendSwitch('use-angle', 'd3d11');
+  app.commandLine.appendSwitch('enable-accelerated-2d-canvas');
+  app.commandLine.appendSwitch('use-angle', 'd3d9');
   
+  // Timer ->
+
+  app.commandLine.appendSwitch("raise-timer-frequency");
+  app.commandLine.appendSwitch("disable-background-timer-throttling");
+
   // Networking ->
 
   app.commandLine.appendSwitch("disable-background-networking");
@@ -36,31 +39,43 @@ function switches () {
 
   app.commandLine.appendSwitch("no-sandbox");
   app.commandLine.appendSwitch('high-dpi-support','1');
-  app.commandLine.appendSwitch('enable-javascript-harmony');
+  app.commandLine.appendSwitch('javascript-harmony');
+
+  // v8 ->
+
+  app.commandLine.appendSwitch("disable-v8-idle-tasks");
   app.commandLine.appendSwitch('enable-future-v8-vm-features');
-  app.commandLine.appendSwitch('disable-gpu-sandbox');
+  app.commandLine.appendSwitch("v8-cache-options","code");
 
-  // Unnecessary Stuff ->
+  // WASM ->
 
-  app.commandLine.appendSwitch("disable-breakpad");
-  app.commandLine.appendSwitch("disable-component-update");
-  app.commandLine.appendSwitch("disable-print-preview");
-  app.commandLine.appendSwitch("disable-metrics");
-  app.commandLine.appendSwitch("disable-metrics-repo");
-  app.commandLine.appendSwitch("disable-speech-api");
-  app.commandLine.appendSwitch("disable-web-security");
-  app.commandLine.appendSwitch("disable-hang-monitor");
-  app.commandLine.appendSwitch("disable-2d-canvas-clip-aa");
-  app.commandLine.appendSwitch("disable-bundled-ppapi-flash");
-  app.commandLine.appendSwitch("disable-logging");
+  app.commandLine.appendSwitch("enable-experimental-webassembly-features");
+  app.commandLine.appendSwitch("enable-experimental-webassembly-stack-switching");
+  
+  // Background processes ->
+ 
   app.commandLine.appendSwitch("disable-renderer-backgrounding");
-  app.commandLine.appendSwitch("disable-background-timer-throttling");
-  app.commandLine.appendSwitch("disable-backing-store-limit");
-  app.commandLine.appendSwitch("disable-web-security", "true")
-  app.commandLine.appendSwitch("disable-component-extensions-with-background-pages");
-  app.commandLine.appendSwitch("disable-stack-profiler");
+  app.commandLine.appendSwitch("disable-2d-canvas-clip-aa");
   app.commandLine.appendSwitch('disable-features', 'Vulkan');
+  app.commandLine.appendSwitch('disable-renderer-accessibility');
+  app.commandLine.appendSwitch('disable-composited-antialiasing');
 
+  // Forcing the game to use GPU only! ->
+
+  app.commandLine.appendSwitch('disable-software-rasterizer');
+  app.commandLine.appendSwitch('disable-software-compositing-fallback');
+  app.commandLine.appendSwitch('disable-gpu-process-crash-limit');
+  app.commandLine.appendSwitch('force_high_performance_gpu');
+  app.commandLine.appendSwitch('use-gpu-in-tests');
+  app.commandLine.appendSwitch('disable-low-end-device-mode');
+  
+  // Misc ->
+
+  app.commandLine.appendSwitch("disable-backing-store-limit");
+  app.commandLine.appendSwitch("disable-cookie-encryption");
+  app.commandLine.appendSwitch("disable-stack-profiler");
+  app.commandLine.appendSwitch("enable-background-thread-pool");
+  
 }
 
 function primaryWindow() {
@@ -81,8 +96,7 @@ function primaryWindow() {
     },
     icon: './icon.ico',
   });
-  
-  
+
   switches();
  
   win.webContents.on("before-input-event", (event, input) => {
